a0b78b253bf186d032fa767856b904a17920b5b8
sparsh-m <sparshmisra1@gmail.com>
2020-08-31 01:37:49 +0530

    HeaderNaV

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import { injectGlobal } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
+import HeaderNav from "./HeaderNav";
+import { Transition } from "react-spring";
 
-injectGlobal`
-   *, *:before, *:after {
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
     box-sizing: border-box;
   }
-   body {
+  body {
     font-family: 'Roboto', sans-serif;
     margin: 0;
-    background-color: #34eb46;
-   }
+    background-color: #f1f1f2;
+  }
+`
+
+const StyledApp = styled.div`
+  max-width: 450px;
+  margin: 0 auto;
+  position: relative;
 `;
 
-function App() {
-  return (
-    
+type State = {
+  viewState: string,
+  chatScreenIsVisible: boolean,
+  currentChatId: number,
+  searchTerm: string,
+  searchInputIsvisible: boolean,
+  dropdownIsVisible: boolean
+};
+
+class App extends React.Component<null, State> {
+  state = {
+    viewState: "2",
+    chatScreenIsVisible: false,
+    currentChatId: 0,
+    searchTerm: "",
+    searchInputIsvisible: false,
+    dropdownIsVisible: false
+  };
+
+  changeViewState = (event: SyntheticInputEvent<HTMLDataListElement>) => {
+    const newState = event.target.dataset.nav;
+    this.setState({ viewState: newState });
+  };
+
+  render() {
+    return (
+      <StyledApp>
+        <GlobalStyle />
+        <HeaderNav
+          viewState={this.state.viewState}
+          changeViewState={this.changeViewState}
+        />
+      </StyledApp>
+    );
+  }
 }
 
 export default App;
---
+++ src/HeaderNav.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHeaderNav = styled.nav`
+  background: green;
+  position: sticky;
+  background-color: #075e54;
+  top: 0;
+  z-index: 1;
+  font-size: 0.9em;
+  font-weight: 600;
+  height: 45px;
+  display: flex;
+  letter-spacing: 0.5px;
+  justify-content: space-between;
+  color: white;
+  a {
+    flex-grow: 2
+    padding-top: 15px;
+  }
+  a:first-child {
+    flex-grow: 1
+    padding-top: 10px;
+  }
+`;
+
+const StyledNavItem = styled.a`
+  text-align: center;
+  transition: background-color 1.5s;
+  color: ${props => (props.viewState === props.current ? "white" : "#83afaa")};
+  border-bottom: ${props =>
+    props.viewState === props.current ? "3px solid white" : "none"};
+  &:active {
+    background-color: lightgray;
+    transition: background-color 0.5s;
+    border-bottom: 3px solid white;
+  }
+
+`;
+
+type Props = {
+  changeViewState: Function,
+  viewState: string
+};
+
+const HeaderNav = ({ changeViewState, viewState }: Props) => {
+  return (
+    <StyledHeaderNav>
+      <StyledNavItem
+        data-nav="1"
+        onClick={changeViewState}
+        viewState={viewState}
+        current="1"
+      >
+        CAM
+      </StyledNavItem>
+      <StyledNavItem
+        data-nav="2"
+        onClick={changeViewState}
+        viewState={viewState}
+        current="2"
+      >
+        CHATS
+      </StyledNavItem>
+      <StyledNavItem
+        data-nav="3"
+        onClick={changeViewState}
+        viewState={viewState}
+        current="3"
+      >
+        STATUS
+      </StyledNavItem>
+    </StyledHeaderNav>
+  );
+};
+
+export default HeaderNav;
