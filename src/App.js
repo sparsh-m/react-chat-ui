028e953fd162f8baa40cfe4bdd9c8eaec76b8140
sparsh-m <sparshmisra1@gmail.com>
2020-08-30 23:40:11 +0530

    Installing dependencies and starting the project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { injectGlobal } from "styled-components";
+import styled from "styled-components";
+
+injectGlobal`
+   *, *:before, *:after {
+    box-sizing: border-box;
+  }
+   body {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    background-color: #34eb46;
+   }
+`;
+
+function App() {
+  return (
+    
+}
+
+export default App;
